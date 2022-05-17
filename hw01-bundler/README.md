4c73c3ae61abfc596600e30c8a53a0b9c5cda6af
Sean <sean@Seans-MBP.lan>
2022-05-16 20:48:15 -0700

    Filters extraneous Django files from bundler; migrates to pipenv/Pipfile

---
+++ hw01-bundler/README.md
@@ -0,0 +1,8 @@
+# Homework Bundler
+Bundles homework for easy submission to Canvas
+
+# Using this app...
+
+Place the bundler executable in the same folder as your homework and run the app!
+
+The **bundle** executable will produce a **bundle.docx** file for submission.
--- hw01-bundler/bundle.py
+++ hw01-bundler/bundle.py
@@ -3,6 +3,7 @@ import sys
 from glob import glob
 from docx import Document
 from docx.shared import Inches, Pt
+from termcolor import colored
 
 if getattr(sys, 'frozen', False):
     app_path = os.path.dirname(sys.executable)
@@ -10,17 +11,24 @@ elif __file__:
     app_path = os.path.dirname(__file__)
 os.chdir(app_path)
 
-types = [ '**/*.md', '**/*.toml', '**/*.py', '**/*.htm', '**/*.html', '**/*.csv', '**/*.json', '**/*.xml', '**/*.png', '**/*.jpg' ]
-files = []
+types = [
+    '**/*.md', '**/*.toml', '**/Pipfile',
+    '**/*.py', '**/*.htm', '**/*.html',
+    '**/*.csv', '**/*.json', '**/*.xml',
+    '**/*.png', '**/*.jpg'
+]
 
+files = []
 for t in types: files.extend(glob(t, recursive=True))
 files = sorted(files)
 
 document = Document()
 
+print(colored(f"Bundling files...", 'white', attrs=['bold']))
 for file in files:
-    if 'build' in file or 'dist' in file: continue
-    print(file)
+    if 'build' in file or 'dist' in file or '__init__.py' in file: continue
+    if 'manage.py' in file or 'asgi.py' in file or 'wsgi.py' in file: continue
+    print(' - ' + colored(f"{file}", 'green'))
     document.add_heading(file, 0)
     if '.png' in file or '.jpg' in file:
         document.add_picture(file, width=Inches(6))
