583b91c6cace9414768087ec59e1a3dcd1e0e219
Sean Lickfold <seansbox@gmail.com>
2024-06-06 18:37:50 -0700

    Adds login/logout

--- hw09/movies/urls.py
+++ hw09/movies/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # Empty (root) path goes to the index view
-    path("", views.MovieListView.as_view(), name="index"),
+    path("", views.MovieListView.as_view(), name="home"),
     path("about", views.about, name="about"),
 ]
--- hw09/yummytomatoes/urls.py
+++ hw09/yummytomatoes/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", include("movies.urls")),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
 ]
